bbc3c0ac0293a1a70d5fe9de780ec1261dc8ad9b
ecriahb <ecriahb@gmail.com>
2025-01-14 15:25:37 +0530

    backend updated

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "4.5.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "pipelinerg"
-    storage_account_name = "pipelineext"
-    container_name       = "splunk"
-    key                  = "splunk.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
