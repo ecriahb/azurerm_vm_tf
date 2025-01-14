d964aa7e5c4b6d17b07cdd6dcd6a2c6a347ee720
ecriahb <125296586+ecriahb@users.noreply.github.com>
2025-01-14 10:25:25 +0530

    new vm splunk.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                = "AngularJS-RG-PublicIP"
+  name                = "AngularJS-RG-PublicIP1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
@@ -15,21 +15,21 @@ resource "azurerm_public_ip" "pip" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "AngularJS-RG-VNet"
+  name                = "AngularJS-RG-VNet1"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "AngularJS-RG-Subnet"
+  name                 = "AngularJS-RG-Subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "AngularJS-RG-NIC"
+  name                = "AngularJS-RG-NIC1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -42,7 +42,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "AngularJS-RG-VM"
+  name                = "splunkfwder"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2s"
